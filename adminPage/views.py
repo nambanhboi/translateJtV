62483141d7eac58049edaad3d65d1bbe52e02984
nambanhboi <vuhainam18102003@gmail.com>
2023-07-17 23:07:48 +0700

    ds

--- adminPage/views.py
+++ adminPage/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
+from .serializers import SentenceSerializer, ParagraphSerializer
+from DictionaryJtVApp.models import Sentence, Paragraph
 
 # Create your views here.
+
+class SentenceViewSet(viewsets.ModelViewSet):
+    queryset = Sentence.objects.all()
+    serializer_class = SentenceSerializer
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+    filteret_fields = ('sentenceJV', 'sentenceVN', 'style', 'topic')
+    #các trường tìm kiếm
+    search_fields = ('sentenceJV', 'sentenceVN', 'style', 'topic')
+
+class ParagraphViewSet(viewsets.ModelViewSet):
+    queryset = Paragraph.objects.all()
+    serializer_class = ParagraphSerializer
+    
